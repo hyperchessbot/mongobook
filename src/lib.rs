a73de0f4d92cdc35bf92ca9efc532253d9078617
scalasbtprojects <scalasbtprojects@gmail.com>
2021-01-08 08:13:51 +0100

    Simplify example

--- src/lib.rs
+++ src/lib.rs
@@ -23,16 +23,6 @@
 //!	env_logger::init();
 //!	
 //!	println!("mongobook, under construction");
-//!	
-//!	//println!("mongodb uri {}", env::var("MONGODB_URI").unwrap());
-//!	
-//!	//let client = connect(env_string_or("MONGODB_URI", "")).await?;
-//!	
-//!	//println!("\nmongo client {:?}\n", client);
-//!	
-//!	//let pgn = std::fs::read_to_string("test.pgn")?;
-//!	
-//!	//println!("\n{:?}\n", parse_pgn_to_rust_struct(pgn));
 //!
 //!	let mut mongobook = MongoBook::new();
 //!
